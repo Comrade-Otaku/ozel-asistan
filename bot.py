b7d886ef86e96ac15f2791836bcb16f6f5d8ab69
Çınar Yılmaz <79412062+Comrade-Otaku@users.noreply.github.com>
2021-03-07 21:00:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,21 @@
 import discord
 import time
 from discord.ext import commands
+from tinydb import TinyDB, Query
 
 TOKEN = "bruh"
 
-client = commands.Bot(command_prefix = "lx.")
+client = commands.Bot(command_prefix = "ğ")
 client.remove_command("help")
 
 @client.event
 async def on_ready():
       print("Özel Asistan uyandı.")
-      channel = client.get_channel(817726201514164236)
-      await channel.send('Günaydın ev ahalisi, asistanınız uyandı. Ne isterdiniz? @here')
       
 @client.command()
 async def help(ctx, arg = "1"):
     if(arg=="1"):
-        await ctx.send("```lx.help {Sayfa Numarası} - Komutları listeler\nlx.ping - Botun pingini aktarır.\nlx.ban - Kullanıcıyı yasaklar.\nlx.kick - Kullanıcıyı atar.\nlx.unban - Kullanıcının yasağını kaldırır.\nlx.avatar - Kullanıcının profil fotoğrafını gösterir.```")
+        await ctx.send("```ğhelp {Sayfa Numarası} - Komutları listeler\nğping - Botun pingini aktarır.\nğban - Kullanıcıyı yasaklar.\nğkick - Kullanıcıyı atar.\nğunban - Kullanıcının yasağını kaldırır.\nğavatar - Kullanıcının profil fotoğrafını gösterir.```")
       
 @client.command()
 async def ping(ctx):
@@ -52,5 +51,27 @@ async def avatar(ctx, *,  avamember : discord.Member=None):
     userAvatarUrl = avamember.avatar_url
     await ctx.send(userAvatarUrl)
 
+@commands.has_role("Duyurabilir")
+@client.command()
+async def duyur(ctx, *args):
+	response = ""
+
+	for arg in args:
+		response = response + " " + arg
+	
+	response = response[1:len(response)]
+	await ctx.message.delete()
+	await ctx.send("**Duyuru!**  @here\n```" + response + "```")
+
+@client.command()
+async def attack(ctx, *,  imparatorluk):
+    await ctx.send("```" + imparatorluk + " ülkesine ateş açıldı!\nTanklar, ileri! Yönümüz " + imparatorluk + " İmparatorluğu!```")
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Efendim, ğ?")
+    else:
+        raise error
 
 client.run(TOKEN)
