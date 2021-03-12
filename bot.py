ce81b379df351e4c75fd60059454f771ae94b442
Çınar Yılmaz <79412062+Comrade-Otaku@users.noreply.github.com>
2021-03-12 20:46:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,34 +33,6 @@ async def ping(ctx):
 
 @client.command()
 @commands.guild_only()
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, user: discord.Member, *, reason=None):
-    await user.kick(reason=reason)
-    await ctx.send(f"```{user} başarıyla atıldı.\nSebep: " + reason + "```")
-  
-@client.command()
-@commands.guild_only()
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, user: discord.Member, *, reason=None):
-    await user.ban(reason=reason)
-    await ctx.send(f"```{user} başarıyla yasaklandı.\nSebep: " + reason + "```")
-
-@client.command()
-@commands.guild_only()
-async def unban(ctx, *, member):
-  banned_users = await ctx.guild.bans()
-  member_name, member_discriminator = member.split('#')
-
-  for ban_entry in banned_users:
-    user = ban_entry.user
-  
-  if (user.name, user.discriminator) == (member_name, member_discriminator):
-    await ctx.guild.unban(user)
-    await ctx.send(f"```{user} kişisinin yasağı başarıyla kaldırıldı.```")
-    return
-
-@client.command()
-@commands.guild_only()
 async def avatar(ctx, *,  avamember : discord.Member=None):
     userAvatarUrl = avamember.avatar_url
     await ctx.send(userAvatarUrl)
